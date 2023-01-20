d492c6331aca0bd8961d2579a46e69897359de97
Fani Salamah <fansaid99@gmail.com>
2023-01-20 21:34:24 +0700

    finishing design table monitoring student

--- resources/views/internship/internship-logbooks.blade.php
+++ resources/views/internship/internship-logbooks.blade.php
@@ -22,87 +22,101 @@
             </div>
         </div>
         <section class="section">
-            <div class="d-flex p-1 mb-2">
-                
-                <div class="flex-fill m-1 p-1">
-                    <span style="font-size: 12px;">     
-                    <table>
-                        
-                        <tr>
-                            <td>Nama Siswa&emsp;</td>
-                            <td>:</td>
-                            <td>&emsp;{{ $internships->students->name }}</td>
-                            
-                        </tr>
-                        <tr>
-                            <td>NIS</td>
-                            <td>:</td>
-                            <td>&emsp;{{ $internships->students->nis }}</td>
-                            
-                        </tr>
-                    </table>
-                </span> 
-                </div>
-                <div class="flex-fill m-1 p-1">
-                    <span style="font-size: 12px;">     
-                    <table>
-                        <tr>
-                            <td>Industri&emsp;</td>
-                            <td>:</td>
-                            <td>&emsp;{{ $internships->industries->name }}</td>
-                            
-                        </tr>
-                        <tr>
-                            <td>Alamat&emsp;</td>
-                            <td>:</td>
-                            <td>&emsp;{{ $internships->industries->address }}</td>
-                            
-                        </tr>
-                    </table>
-                </span>
-                </div>
-                <div class="flex-fill m-1 p-1">
-                    <span style="font-size: 12px;">     
-                    @if($internships->advisor_id == 0)
-                        Advisor&emsp;:&emsp;Pembimbing belum diset
-                        @else
-                        Advisor&emsp;:&emsp;{{ $internships->advisors->name }}
-                        
-                    @endif
-                     
-                    </span>
-                </div>
-               
-              
             
-            
-            </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <span style="font-size: 12px;">     
+                            <table>
+                                
+                                <tr>
+                                    <td>Nama Siswa&emsp;</td>
+                                    <td>:</td>
+                                    <td>&emsp;{{ $internships->students->name }}</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>NIS</td>
+                                    <td>:</td>
+                                    <td>&emsp;{{ $internships->students->nis }}</td>
+                                    
+                                </tr>
+                            </table>
+                        </span>
+                        </div>
+
+                        <div class="col-6">
+                            <span style="font-size: 12px;">     
+                                <table>
+                                    <tr>
+                                        <td>Industri&emsp;</td>
+                                        <td>:</td>
+                                        <td>&emsp;{{ $internships->industries->name }}</td>
+                                        
+                                    </tr>
+                                    <tr>
+                                        <td>Alamat&emsp;</td>
+                                        <td>:</td>
+                                        <td>&emsp;{{ $internships->industries->address }}</td>
+                                        
+                                    </tr>
+                                </table>
+                            </span>
+                        </div>
+
+                        <div class="col">
+                            <span style="font-size: 12px;">     
+                                @if($internships->advisor_id == 0)
+                                    Advisor&emsp;:&emsp;Pembimbing belum diset
+                                    @else
+                                    Advisor&emsp;:&emsp;{{ $internships->advisors->name }}
+                                    
+                                @endif
+                                 
+                                </span>
+                            
+                        </div>
+                    </div>
+
+                </div>
+       <br>         
+       
             <div class="card">
                 
                 <div class="card-body">
                    
                  
-                     
-                    {{-- <table class="table table-striped" id="">
+                   <button class="btn btn-primary mb-2" type=""> Export </button>
+                   <span style="font-size: 14px;">     
+                    <table class="table table-striped" id="">
+                        
                         <thead>
                             <tr>
                                 <th>No. </th>
-                                <th>Nama Siswa</th>
-                                <th>Jurusan</th>
-                                <th>Industri</th>
-                                <th>Alamat</th>
-                                <th>Advisor</th>
+                                <th>Tanggal</th>
+                                <th>Kegiatan</th>
+                                <th>Lampiran</th>
+                                <th>Catatan</th>
+                                
                             </tr>
                         </thead>
                         <tbody>
-                          
-
+                            
+                            <tr>
+                                <td> 1</td>
+                                <td width="15%"> 12 Januari 2023</td>
+                                <td width="40%"> Mendengarkan diskusi dari mentor mengenai design sprint dan metode pengembangan scrum. Daily scrum dan kenalan dengan
+                                    scrum master
+                                </td>
+                                <td> Lampiran.pdf</td>
+                                <td width="20%"> Belum ada catatan</td>
                                 
                             </tr>
                             
                         </tbody>
-                        
-                    </table> --}}
+                    
+                    </table>
+                </span>
 
 
 
