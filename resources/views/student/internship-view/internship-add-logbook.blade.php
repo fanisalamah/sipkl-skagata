35ee439417e19379b90c06d98c005ed9eb44af2a
Fani Salamah <fansaid99@gmail.com>
2023-03-14 22:02:34 +0700

    update

--- resources/views/student/internship-view/internship-add-logbook.blade.php
+++ resources/views/student/internship-view/internship-add-logbook.blade.php
@@ -27,44 +27,51 @@
                   <div class="card-body">
                     <form class="form">
                       <div class="row">
+
                         <div class="col-md-6 col-12">
                           <div class="form-group">
                             <label for="date">Tanggal</label>
                             <input type="date" id="date" class="form-control" placeholder="Pilih tanggal" name="date">
                           </div>
                         </div>
+
                         <div class="col-md-6 col-12">
                           <div class="form-group">
                             <label for="last-name-column">Lampiran</label>
                             <div class="mb-3">                                
                                 <input class="form-control" type="file" id="formFile">
-                              </div>
-                                  
-                              
+                              </div> 
                           </div>
                         </div>
-                        <div class="col-md-6 col-12">
-                          <div class="form-group">
-                            <label for="activity">Uraian Kegiatan</label>
-                            <div class="form-group with-title mb-3">
-                                <textarea class="form-control" id="activity" style="height: 168px;"></textarea>
-                                
-                              </div>
-                                </div>
-                              </div>
+
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-6 col-12">
+                                <div class="form-group">
+                                  <label for="activity">Uraian Kegiatan</label>
+                                  <div class="form-group with-title mb-3">
+                                      <textarea class="form-control" id="activity" style="height: 168px;"></textarea>
+                                      
+                                    </div>
+                                      </div>
+                                    </div>
+                                    <div class="col-md-6 col-12">
+                                        <button type="submit" class="btn btn-primary me-1 mb-1">
+                                            Submit
+                                          </button>
+                                          <button type="reset" class="btn btn-light-secondary me-1 mb-1">
+                                            Reset
+                                          </button>
+                                      </div>
+                        </div>
+
+
                           </div>
                         </div>
                         
-                        <div class="form-group col-12">
-                   
-                        </div>
                         <div class="col-12 d-flex justify-content-end">
-                          <button type="submit" class="btn btn-primary me-1 mb-1">
-                            Submit
-                          </button>
-                          <button type="reset" class="btn btn-light-secondary me-1 mb-1">
-                            Reset
-                          </button>
+                          
                         </div>
                       </div>
                     </form>
